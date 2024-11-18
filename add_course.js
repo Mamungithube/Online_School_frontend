74186515b78f04022eca9276688884c6131d53b0
mamungithube <orroshidmdmamun50@gmail.com>
2024-11-18 16:54:23 +0600

    edit

--- add_course.js
+++ add_course.js
@@ -1,19 +1,16 @@
 const addCourse = (event) => {
     event.preventDefault();
-
     const form = document.getElementById("addCourseForm");
     const formData = new FormData(form);
-
     fetch("http://127.0.0.1:8000/Course/list/", {
         method: "POST",
         headers: {
             // "Authorization": `Bearer ${token}`, // if using token
         },
-        body: formData,  // Send form data as 'multipart/form-data'
+        body: formData, 
     })
         .then((res) => {
             if (!res.ok) {
-                // If response is not OK, attempt to read as text (HTML error page)
                 return res.text().then((text) => {
                     throw new Error(text);
                 });
